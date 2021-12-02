495d180f250cae487a247cba4963c551b13c48a0
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2021-12-02 23:18:56 +0200

    second day puzzles

---
+++ 02/second/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+
+    let mut depth = 0;
+    let mut aim = 0;
+    let mut horizontal_position = 0;
+    for line in contents.lines() {
+        let cmd = line.split(" ").collect::<Vec<&str>>();
+        match *cmd.first().unwrap() {
+            "forward" => {
+                horizontal_position += cmd[1].parse::<i32>().unwrap();
+                depth += aim * cmd[1].parse::<i32>().unwrap()
+            },
+            "up" => aim -= cmd[1].parse::<i32>().unwrap(),
+            "down" => aim += cmd[1].parse::<i32>().unwrap(),
+            &_ => panic!("Unknown command"),
+        }
+    }
+
+    println!("position {} depth {}", horizontal_position, depth);
+    println!("{}", horizontal_position * depth);
+    
+}
